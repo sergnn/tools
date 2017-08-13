9c83ad1d7b3e52d559961b943180c0370b8224cc
moocher <moocher@moocher.ru>
2017-08-13 16:50:10 +0300

    fixed exception type of sights_ids changed to set()

--- altertravel_parser/altertravel_parser.py
+++ altertravel_parser/altertravel_parser.py
@@ -8,6 +8,7 @@ import sys
 import urllib
 import urllib2
 
+import lxml
 import progressbar
 from bs4 import BeautifulSoup
 from lxml import etree
@@ -25,7 +26,7 @@ arguments = arguments.parse_args()
 # Loading results for the search by the tag
 print('Parsing pages for the results')
 tag = arguments.tag.encode('utf-8')
-sights_ids = []
+sights_ids = set()
 page = 0
 while True:
     print('Page {}'.format(page))
@@ -38,7 +39,7 @@ while True:
 
     for sight in sights:
         sight_id = re.search('id=([0-9]*)', sight.a.get('href')).group(1)
-        sights_ids.append(sight_id)
+        sights_ids.add(sight_id)
     page += 1
 
 cache = []
@@ -49,17 +50,21 @@ if os.path.exists('cache'):
 kml_folder = Kml.Folder(Kml.name(tag.decode('utf-8-sig')))
 sights_count = len(sights_ids)
 print('Parsing {} KMLs'.format(sights_count))
-bar = progressbar.ProgressBar(maxval=sights_count,
+bar = progressbar.ProgressBar(maxval=sights_count * 2,
                               widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 bar.start()
 new_point_count = 0
 for index, sight_id in enumerate(sights_ids):
     # Loading and parsing KMLs
     kml_str = urllib2.urlopen('{}/generate_kml.php?id={}'.format(SITE, sight_id))
-    root = parser.parse(kml_str).getroot()
-    point_name = root.Document.Placemark.name.text
-    point_coordinates = root.Document.Placemark.Point.coordinates.text
-    bar.update(index)
+    try:
+        root = parser.parse(kml_str).getroot()
+        point_name = root.Document.Placemark.name.text
+        point_coordinates = root.Document.Placemark.Point.coordinates.text
+    except lxml.etree.XMLSyntaxError:
+        continue
+
+    bar.update(index * 2)
 
     if point_coordinates in cache:
         continue
@@ -71,7 +76,7 @@ for index, sight_id in enumerate(sights_ids):
     for div in description.find_all('div'):
         div.extract()
     description = Kml.description(description.p.text.strip() if description.p else '')
-    bar.update(index + 1)
+    bar.update(index * 2 + 1)
     point = Kml.Placemark(Kml.name(point_name), Kml.Point(Kml.coordinates(point_coordinates)), description)
     kml_folder.append(point)
     cache.append(point_coordinates)
