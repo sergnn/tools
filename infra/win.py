77430f2423051f40afc5fb2498a2d03c12d5cbc8
moocher <moocher@moocher.ru>
2018-01-30 23:24:12 +0300

    Windows updates enumeration and correct Windows ID definition

--- infra/win.py
+++ infra/win.py
@@ -1,28 +1,43 @@
 import os
-import platform
+import re
+import winreg
+
+import win32com.client
 
 
 def win_bits() -> int:
     return 64 if "PROGRAMFILES(X86)" in os.environ else 32
 
 
-def win_ver():
-    ver = platform.version().split('.')
-    major_ver = int(ver[0])
-    build = int(ver[2])
+def win_release_id() -> int:
+    key = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    val = r"ReleaseID"
+    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key)
+    releaseId = int(winreg.QueryValueEx(key, val)[0])
+    winreg.CloseKey(key)
+    return releaseId
 
-    str_build = ''
-    if build <= 10240:
-        str_build = 'th1'
-    elif build <= 10586:
-        str_build = 'th2'
-    elif build <= 14393:
-        str_build = 'rs1'
-    elif build <= 15063:
-        str_build = 'rs2'
-    elif build <= 16299:
-        str_build = 'rs3'
-    elif build <= 17046:
-        str_build = 'rs4'
 
-    return major_ver, build, str_build
+def enum_winupdates():
+    wua = win32com.client.Dispatch('Microsoft.Update.Session')
+    update_searcher = wua.CreateUpdateSearcher()
+    update_searcher.IncludePotentiallySupersededUpdates = True
+    history_count = update_searcher.GetTotalHistoryCount()
+    search_result = update_searcher.QueryHistory(0, history_count)
+    updates = []
+    for update in search_result:
+        if update.ResultCode == 2 and update.Operation == 1:
+            try:
+                result = re.search(r'KB\d+', update.Title)
+                updates.append(result.group(0))
+            except AttributeError:
+                pass
+    return updates
+
+
+patches = {1709: {'Spectre&Meltdown': 'KB4051892', 'Petya': 'KB0000'}, }
+
+if __name__ == '__main__':
+    required = patches[win_release_id()]
+    installed = enum_winupdates()
+    print([(v, p) for v, p in required.items() if p not in installed])
